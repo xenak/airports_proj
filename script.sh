8ae38ce9bee78993460851ca36f2d4827b5ec6c3
xenak <dalekbluebell@gmail.com>
2018-12-17 00:16:18 +0300

    change directories

--- script.sh
+++ script.sh
@@ -1,20 +1,20 @@
-rm ~/homework/info.csv
-printf 'Airport\tWind (MPH)\tTemperature (C)\tStatus\n' > ~/homework/info.csv
+rm ~/info.csv
+printf 'Airport\tWind (MPH)\tTemperature (C)\tStatus\n' > ~/info.csv
 
 
-for airports in $(cat ~/homework/airports.txt); do
-   curl http://tgftp.nws.noaa.gov/data/observations/metar/decoded/$airports.TXT > ~/homework/text.txt;
-   if grep -q  "MPH" ~/homework/text.txt; then
-      grep "Wind:" ~/homework/text.txt > ~/homework/wind.txt
-      if grep -q "to" ~/homework/wind.txt; then
-         WIND=$(grep -oP "to\s+\K\w+" ~/homework/wind.txt);
+for airports in $(cat ~/airports.txt); do
+   curl http://tgftp.nws.noaa.gov/data/observations/metar/decoded/$airports.TXT > ~/text.txt;
+   if grep -q  "MPH" ~/text.txt; then
+      grep "Wind:" ~/text.txt > ~/wind.txt
+      if grep -q "to" ~/wind.txt; then
+         WIND=$(grep -oP "to\s+\K\w+" ~/wind.txt);
       else
-          WIND=$(grep -oP "at\s+\K\w+" ~/homework/wind.txt);
+          WIND=$(grep -oP "at\s+\K\w+" ~/wind.txt);
       fi 
    else 
       WIND=$(echo "0");
    fi
-   TEMP=$(grep "Temperature" ~/homework/text.txt  | awk '{print $4}'  | cut -c 2-);
+   TEMP=$(grep "Temperature" ~/text.txt  | awk '{print $4}'  | cut -c 2-);
    if [ ${TEMP%.*} -gt "-3"  ]; then
       if [ $WIND -gt 15 ]; then
          MESS=$(echo "Be careful!");
@@ -24,8 +24,8 @@ for airports in $(cat ~/homework/airports.txt); do
    else
       MESS=$(echo "Be careful!");
    fi
-   printf "$airports\t$WIND\t$TEMP\t$MESS\n" >> ~/homework/info.csv
-done < ~/homework/airports.txt
+   printf "$airports\t$WIND\t$TEMP\t$MESS\n" >> ~/info.csv
+done < ~/airports.txt
 
-rm ~/homework/wind.txt
-rm ~/homework/text.txt
+rm ~/wind.txt
+rm ~/text.txt
